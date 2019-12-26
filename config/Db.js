64cc5e88f59b8579cd8504c7760c9d5e6ede781d
Muhammed Jinas <57747927+JinasMuhammed@users.noreply.github.com>
2019-12-26 13:19:10 +0530

    Add files via upload

---
+++ config/Db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb+srv://jinas:test1234@jinas-ookgp.mongodb.net/DB_Employee?retryWrites=true&w=majority'
+};
---
+++ config/dist/Db.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+  DB: 'mongodb+srv://jinas:test1234@jinas-ookgp.mongodb.net/DB_Employee?retryWrites=true&w=majority'
+};
